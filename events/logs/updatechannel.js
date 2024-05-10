4db69427663e8d1a80eff7259e87190b98267c6a
Shinetsu_Ruwin <110337782+Ruwin-dsc@users.noreply.github.com>
2024-05-10 10:13:29 +0200

    Fix updatechannel.js

--- events/logs/updatechannel.js
+++ events/logs/updatechannel.js
@@ -3,7 +3,7 @@ const config = require('../../config.json')
 module.exports = {
   name: "channelUpdate",
   async execute(oldChannel, newChannel, bot) {
-    if(oldChannel.name == newChannel.name) return
+    let verify = false
     bot.db.query(`SELECT * FROM logs WHERE guildId = "${newChannel.guild.id}"`, async (err, req) => {
         const embed = new Discord.EmbedBuilder()
         .setColor("Orange")
@@ -11,9 +11,15 @@ module.exports = {
         .setTitle(`Modification d'un salon`)
         .setDescription(`Salon modifié : ${newChannel} \`${oldChannel.name}\`.`)
         .setFooter({ text: `ID du salon : ${newChannel.id}` })
-        .addFields(
-            { name: "Nom :", value: `> \`${oldChannel.name}\` ➔ \`${newChannel.name}\` `}
-        )
+   
+        if(oldChannel.name !== newChannel.name) {
+            verify = true
+            embed.addFields({ name: "Nom :", value: `> \`${oldChannel.name}\` ➔ \`${newChannel.name}\` `})
+        }
+        if(oldChannel.nsfw !== newChannel.nsfw) {
+            verify = true
+            embed.addFields({ name: "**Salon soumis à une limite d'âge :**", value: `> \`${oldChannel.nsfw == true ? "Oui" : "Non"}\` ➔ \`${newChannel.nsfw == true ? "Oui" : "Non"}\` `})
+        }
         const channel2 = newChannel.guild.channels.cache.get(req[0].channelGuild)
         if(channel2) channel2.send({ embeds: [embed]})
     })
